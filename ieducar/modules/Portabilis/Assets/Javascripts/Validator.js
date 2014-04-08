39a563b0d198add2c223d661788b68b797ce3032
Carlos M. dos Santos <carlos-morais.santos@serpro.gov.br>
2014-04-08 17:39:57 -0300

    Resolvendo um caso de validação de CPF.

--- ieducar/modules/Portabilis/Assets/Javascripts/Validator.js
+++ ieducar/modules/Portabilis/Assets/Javascripts/Validator.js
@@ -34,6 +34,22 @@ var validationUtils = {
     if (cpf.length != 11)
       return false;
 
+    var blacklist = Array(
+        '12345678909',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999',
+        '00000000000'
+    );
+    if (blacklist.indexOf(cpf) > -1) 
+       return false;
+
     var soma;
     var resto;
 
