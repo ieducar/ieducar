22d4220ce0867553fcfe6eb0a42932590eb95d54
Carlos M <carlos-morais.santos@serpro.gov.br>
2015-01-30 18:11:37 -0200

    Adicionando diário de classe.
    Change-Id: I3665f9c5b90940fa4a7f39cb99ce75f3d4856ceb

---
+++ ieducar/misc/database/deltas/serpro/20150124-insere-menu-diario-classe.sql
@@ -0,0 +1,61 @@
+INSERT INTO portal.menu_submenu (
+  cod_menu_submenu, 
+  ref_cod_menu_menu, 
+  cod_sistema, 
+  nm_submenu, 
+  arquivo, 
+  title, 
+  nivel
+ ) VALUES (
+  999700,
+  55,
+  2,
+  'Diário de Classe',
+  'module/Reports/DiarioClasse',
+  'Diário de Classe',
+  3
+);
+
+INSERT INTO pmicontrolesis.menu (
+  cod_menu, 
+  ref_cod_menu_pai, 
+  tt_menu, 
+  ord_menu, 
+  alvo, 
+  suprime_menu, 
+  ref_cod_tutormenu, 
+  ref_cod_ico
+ ) VALUES (
+  999700,
+  21127,
+  'Diários',
+  4,
+  '_self',
+  1,
+  15,
+  21
+);
+
+INSERT INTO pmicontrolesis.menu (
+  cod_menu, 
+  ref_cod_menu_submenu, 
+  ref_cod_menu_pai, 
+  tt_menu, 
+  ord_menu, 
+  caminho, 
+  alvo, 
+  suprime_menu, 
+  ref_cod_tutormenu, 
+  ref_cod_ico
+ ) VALUES (
+  999701,
+  999700,
+  999700,
+  'Diario de Classe',
+  1,
+  'module/Reports/DiarioClasse',
+  '_self',
+  1,
+  15,
+  190
+);
